be51a208dbd3fa072f8a833473fde85e8e2de77c
Jeremias Roma <jeremias-dnx@dnx.solutions>
2023-03-17 12:20:11 +1100

    Upgrade external secrets module from the deprecated kubernetes-external-secrets to external secrets operator
    https://github.com/external-secrets/kubernetes-external-secrets/issues/864

--- helm.tf
+++ helm.tf
@@ -1,50 +1,15 @@
-resource "helm_release" "kubernetes_external_secrets" {
-  depends_on = [var.mod_dependency, kubernetes_namespace.kubernetes_external_secrets]
+resource "helm_release" "external_secrets" {
+  depends_on = [var.mod_dependency, kubernetes_namespace.external_secrets]
   count      = var.enabled ? 1 : 0
   name       = var.helm_chart_name
   chart      = var.helm_chart_release_name
   repository = var.helm_chart_repo
   version    = var.helm_chart_version
-  namespace  = var.namespace
-
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
-
-  set {
-    name  = "env.AWS_REGION"
-    value = var.secrets_aws_region
-  }
-
-  set {
-    name  = "env.LOG_LEVEL"
-    value = var.log_level
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = var.service_account_name
-  }
+  namespace  = kubernetes_namespace.external_secrets[0].id
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.kubernetes_external_secrets[0].arn
-  }
-
-  set {
-    name  = "securityContext.fsGroup"
-    value = 65534
-  }
-
-  set {
-    name  = "customResourceManagerDisabled"
-    value = true
+    value = aws_iam_role.external_secrets[0].arn
   }
 
   values = [
--- namespace.tf
+++ namespace.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_namespace" "kubernetes_external_secrets" {
+resource "kubernetes_namespace" "external_secrets" {
   depends_on = [var.mod_dependency]
   count      = (var.enabled && var.create_namespace && var.namespace != "kube-system") ? 1 : 0
 
