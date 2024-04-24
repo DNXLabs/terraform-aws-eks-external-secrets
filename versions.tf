3f37525abeee9ad6fcb4b215ec4f2b2dee4791cc
Lucas de Macedo <122332519+lucasmacedot@users.noreply.github.com>
2024-04-24 19:30:01 -0400

    Remove max version constraint on providers

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.13"
 
   required_providers {
-    aws        = ">= 3.13, < 4.0"
-    helm       = ">= 1.0, < 3.0"
-    kubernetes = ">= 1.10.0, < 3.0.0"
+    aws        = ">= 3.13"
+    helm       = ">= 1.0"
+    kubernetes = ">= 1.10.0"
   }
 }
