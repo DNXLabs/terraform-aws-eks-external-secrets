8e0c8d5d7fa5f08748966ca6c4e9e8c8e4f399ab
lucasmacedot <lucas.macedo@dnx.solutions>
2024-08-01 19:26:58 -0400

    Remove versions limits on providers

--- _versions.tf
+++ _versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws        = ">= 3.13"
-    helm       = ">= 1.0, < 3.0"
-    kubernetes = ">= 1.10.0, < 3.0.0"
+    helm       = ">= 1.0"
+    kubernetes = ">= 1.10.0"
   }
 }
